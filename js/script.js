f7320cef6dff225d2a621e56a3f10b0dd47222e2
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-04-18 23:30:40 +0300

    Add jquery to portfolio for toggle effect

--- js/script.js
+++ js/script.js
@@ -2,4 +2,25 @@ $(document).ready(function(){
     $("#work1").hover(function(){
         $(".work1").toggle();
     });
+    $("#work2").hover(function(){
+        $(".work2").toggle();
+    });
+    $("#work3").hover(function(){
+        $(".work3").toggle();
+    });
+    $("#work4").hover(function(){
+        $(".work4").toggle();
+    });
+    $("#work5").hover(function(){
+        $(".work5").toggle();
+    });
+    $("#work6").hover(function(){
+        $(".work6").toggle();
+    });
+    $("#work7").hover(function(){
+        $(".work7").toggle();
+    });
+    $("#work8").hover(function(){
+        $(".work8").toggle();
+    });
 });
